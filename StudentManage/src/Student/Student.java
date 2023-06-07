89e50eb5bdfe28fbe031ed0c576859e4c481cce3
your name <pastover143@gmail.com>
2023-06-07 16:28:07 +0700

    upfdat

---
+++ StudentManage/src/Student/Student.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Student;
+
+/**
+ *
+ * @author Yudlee
+ */
+public class Student {
+
+    private String fullName, gender;
+    private int age;
+    private String email, phone;
+
+
+    public Student() {
+    }
+
+    public Student(String fullName, String gender, int age, String email, String phone) {
+        this.fullName = fullName;
+        this.gender = gender;
+        this.age = age;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return fullName + ", " + gender + ", " + age + ", " + email + ", " + phone;
+    }
+
+}
